d70f9ed74eadf687a14ed38d77d58cb54bff51be
Hitanya-Srivastava <83904224+Hitanya-Srivastava@users.noreply.github.com>
2022-01-21 12:53:07 +0530

    Time: 68 ms (6.78%), Space: 14.4 MB (16.86%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -0,0 +1,32 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+  
+    def isSymmetric(self, root):
+        if root is None:
+            return True
+        stack = []
+        stack.append(root.left)
+        stack.append(root.right)
+
+        while stack:
+            p, q = stack.pop(), stack.pop()
+
+            if p is None and q is None:
+                continue
+
+            if p is None or q is None or p.val != q.val:
+                return False
+
+            stack.append(p.left)
+            stack.append(q.right)
+
+            stack.append(p.right)
+            stack.append(q.left)
+
+        return True
